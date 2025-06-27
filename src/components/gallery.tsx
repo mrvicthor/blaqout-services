1f6ad8ef3a4ed456be2820cdfb885fd5e4cc6d26
Victor Eleanya <victoreleanya89@gmail.com>
2025-06-27 20:25:09 +0100

    fix(gallery.tsx): changed button to anchor tag

--- src/components/gallery.tsx
+++ src/components/gallery.tsx
@@ -1,7 +1,6 @@
 import { motion } from "motion/react";
 import financeImage from "../assets/finance.png";
 import taskManagerImage from "../assets/task-manager-preview.png";
-import bookingImage from "../assets/bookings.png";
 import { FiArrowUpRight } from "react-icons/fi";
 import photoSnapImage from "../assets/photosnap.png";
 import audioPhileImage from "../assets/audiophile.png";
@@ -9,18 +8,39 @@ import aboutMeImage from "../assets/portLogo.jpg";
 import movieApp from "../assets/web-ent.png";
 import iptrackerImage from "../assets/iptrac_proj.png";
 import formImage from "../assets/multi-step-form.png";
+import calendarImage from "../assets/calendar.png";
 import { Link } from "react-scroll";
 
 const Gallery = () => {
   const firstColumn = [
-    { id: 1, preview: financeImage, name: "personal finance" },
-    { id: 2, preview: taskManagerImage, name: "task manager" },
-    { id: 3, preview: bookingImage, name: "booking system" },
+    {
+      id: 1,
+      preview: financeImage,
+      name: "personal finance",
+      url: "https://personal-finance-app-orpin.vercel.app/login",
+    },
+    {
+      id: 2,
+      preview: taskManagerImage,
+      name: "task manager",
+      url: "https://task-manager-app-sandy-theta.vercel.app/",
+    },
+    {
+      id: 3,
+      preview: calendarImage,
+      name: "booking system",
+      url: "https://calendar-app-alpha-jade.vercel.app/",
+    },
   ];
 
   const secondColumn = [
-    { id: 1, preview: aboutMeImage, name: "blaqout" },
-    { id: 2, preview: photoSnapImage, name: "photosnap studio" },
+    { id: 1, preview: aboutMeImage, name: "blaqout", url: "/" },
+    {
+      id: 2,
+      preview: photoSnapImage,
+      name: "photosnap studio",
+      url: "https://photosnap-multi-page-website-nu.vercel.app/",
+    },
     {
       id: 3,
       preview: audioPhileImage,
@@ -61,12 +81,15 @@ const Gallery = () => {
                 className="h-full object-cover w-full rounded-md grayscale hover:grayscale-0 transition duration-500"
               />
               <div className="absolute bottom-4 w-full px-6">
-                <button className="w-full border border-white/30 rounded-2xl py-2 bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] flex items-center justify-center gap-2">
+                <a
+                  href={column.url}
+                  className="w-full border border-white/30 rounded-2xl py-2 bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] flex items-center justify-center gap-2"
+                >
                   <span className="text-black/20 capitalize">
                     view casestudy
                   </span>{" "}
                   <FiArrowUpRight className="text-black/20" />
-                </button>
+                </a>
               </div>
             </div>
           ))}
@@ -101,14 +124,16 @@ const Gallery = () => {
                   className="h-full object-cover w-full rounded-md grayscale hover:grayscale-0 transition duration-500"
                 />
                 <div className="absolute bottom-4 w-full px-6">
-                  <button className="w-full border border-white/30 rounded-2xl py-2 bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] flex items-center justify-center gap-2">
-                    <>
-                      <span className="text-black/20 capitalize">
-                        view casestudy
-                      </span>
-                      <FiArrowUpRight className="text-black/20" />
-                    </>
-                  </button>
+                  <a
+                    href={column.url}
+                    target="_blank"
+                    className="w-full border border-white/30 rounded-2xl py-2 bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] flex items-center justify-center gap-2"
+                  >
+                    <span className="text-black/20 capitalize">
+                      view casestudy
+                    </span>
+                    <FiArrowUpRight className="text-black/20" />
+                  </a>
                 </div>
               </div>
             )
@@ -124,12 +149,15 @@ const Gallery = () => {
                 className="h-full w-auto rounded-md grayscale hover:grayscale-0 transition duration-500"
               />
               <div className="absolute bottom-4 w-full px-6">
-                <button className="w-full border border-white/30 rounded-2xl py-2 bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] flex items-center justify-center gap-2">
+                <a
+                  href={column.url}
+                  className="w-full border border-white/30 rounded-2xl py-2 bg-white/10 backdrop-blur-md hover:bg-white/20 transition-all duration-300 shadow-lg hover:shadow-xl hover:scale-[1.02] active:scale-[0.98] flex items-center justify-center gap-2"
+                >
                   <span className="capitalize text-black/20">
                     view casestudy
                   </span>{" "}
                   <FiArrowUpRight className="text-black/20" />
-                </button>
+                </a>
               </div>
             </div>
           ))}
